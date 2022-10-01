1c2254bdc8c23236620057c55c3c2fd920471087
Danny Shen <shenfive@gmail.com>
2022-10-01 14:15:47 +0800

    show addressbook

--- a209HelloTableView/AViewController.swift
+++ a209HelloTableView/AViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
+    var addbook:[[String:String]] = []
 
     @IBOutlet weak var oneTableView: UITableView!
     
@@ -20,25 +20,29 @@ class AViewController: UIViewController, UITableViewDelegate, UITableViewDataSou
         oneTableView.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defult = UserDefaults.standard
+        addbook = defult.value(forKey: "addressBook") as? Array<[String:String]> ?? []
+    }
+    
+    
     //MARK:TableView Dalegate and Data Source
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return addbook.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "thereuseid") as! MyTableViewCell
         
-        cell.name.text = "hahaha"
-        
-        cell.phone.text = "call me baby"
+        cell.name.text = addbook[indexPath.row]["name"]
+        cell.phone.text =  addbook[indexPath.row]["phone"]
         
         return cell
     }
